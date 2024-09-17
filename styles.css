7fee210ff1f94e6ee128ef106b0d1948fcd8ea91
adielgp <adielgp@hotmail.com>
2024-09-16 21:19:05 -0500

    Modificar styles.css

--- styles.css
+++ styles.css
@@ -1,1 +1,17 @@
-
+table, th, td {
+    border: 1px solid black;
+}
+.div1{
+    position: fixed;
+    background-color: rgb(255, 255, 255);
+    width: 90%;
+    display: grid;
+    gap: 1px;
+    grid-template-columns: repeat(2, 1fr);
+}
+.div2{
+    width: 150%;
+}
+.div3{
+    padding: 80px;
+}
