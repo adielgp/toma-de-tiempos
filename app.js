f2dffb1e027de29aa6813179b9defe3a6c5c179e
adielgp <90361452+adielgp@users.noreply.github.com>
2024-12-24 08:46:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -70,8 +70,8 @@ function actualizar_tabla(){
     tabla_tiempos.replaceChildren();
     tabla_tiempos.appendChild(tblHeader);
     tabla_tiempos.appendChild(tblBody);
-    tabla_tiempos.rows[datoMinimo-1].cells[0].contentEditable=true;
-    tabla_tiempos.rows[datoMinimo-1].cells[0].focus();
+    //tabla_tiempos.rows[datoMinimo-1].cells[0].contentEditable=true;
+    //tabla_tiempos.rows[datoMinimo-1].cells[0].focus();
     body.appendChild(tabla_tiempos);
     label_datos.textContent=number_dates;
 }
